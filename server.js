60655cd45b6b8381d67251e72bf194300767466f
murilo.victor@unicid.edu.br <mvictor01*>
2019-11-01 14:55:07 -0300

    Ajuste Server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`$/dist/$`));
- 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`$/dist/$/index.html`));
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist/oufyr'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/oufyr/index.html'));
 });
- 
+
 app.listen(process.env.PORT || 8080);
